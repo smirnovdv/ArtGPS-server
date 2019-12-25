186e838aea2ead51fcc5e538cce64f133a51e410
smirnovdv <volmondo@gmail.com>
2019-12-25 12:27:52 -0600

    style corrections

--- server.js
+++ server.js
@@ -1,20 +1,19 @@
 const express = require('express');
 const app = express();
-var cors = require('cors')
+const cors = require('cors')
+const port = process.env.PORT || 3001;
 
-let port = process.env.PORT || 3001;
-
-app.use(cors())
+app.use(cors());
 
 //db connection
 const { Client } = require('pg');
-let connectionObject = {
-    host : "ec2-107-20-239-47.compute-1.amazonaws.com",
-    database : "dcqcqfuvh1n779",
-    ssl: "any", 
-    port : 5432,
-    user : "cxngcbwrxsmpac",
-    password : "80c6f4106a03155d0ef38f3d31039e40590ab4ec34fc6ceba9d7354f473a94e4"
+const connectionObject = {
+  host : "ec2-107-20-239-47.compute-1.amazonaws.com",
+  database : "dcqcqfuvh1n779",
+  ssl: "any", 
+  port : 5432,
+  user : "cxngcbwrxsmpac",
+  password : "80c6f4106a03155d0ef38f3d31039e40590ab4ec34fc6ceba9d7354f473a94e4"
 };
 
 const client = new Client(connectionObject);
@@ -26,65 +25,39 @@ client.connect()
   console.error('connection error', err.stack)
 });
 
-//handling CORS
-// app.use((req,res,next) => {
-//     res.header('Access-Control-Allow-Origin','*');
-    
-// });
-
 //handling requests
 app.get('/get_challenge',(req,res)=>{
-    let query = `SELECT * FROM artworks
-                 ORDER BY RANDOM() LIMIT 3
-                ` 
-    client.query(query, function(err, data) {
-      console.log(err,data.rows);
-      res.send(data.rows);
-      
-    });
-
+  const query = `SELECT * FROM artworks
+                ORDER BY RANDOM() LIMIT 3
+              ` 
+  client.query(query, function(err, data) {
+    console.log(err,data.rows);
+    res.send(data.rows);
+  });
 });
 
 app.get('/get_inspiration',(req,res)=>{
-  let query = `SELECT * FROM modern_artists
-               WHERE id = ${req.query.id}
-              ` 
-  console.log(query)        
+  const query = `SELECT * FROM modern_artists
+                 WHERE id = ${req.query.id}
+                ` 
   client.query(query, function(err, data) {
     console.log(err,data.rows);
     res.send(data.rows);
   });
-
 });
 
 app.get('/get_test',(req,res)=>{
-  let query = `SELECT * FROM modern_artists
+  const query = `SELECT * FROM modern_artists
                ORDER by random() 
                LIMIT 2;
               ` 
-  console.log(query)
   client.query(query, function(err, data) {
     res.send(data.rows);
   });
 
 });
 
-app.get('/challenge_leaderboard',(req,res)=>{
-  let query = `SELECT * FROM users
-               LIMIT 5
-              ` 
-  client.query(query, function(err, data) {
-    console.log(err,data.rows);
-    res.send(data.rows);
-    
-  });
-})
-
-app.post('/challenge_leaderboard'),(req,res)=>{
-  // challenge_leaderboard  
-}
-
-
+//server setup
 app.listen(port, ()=>{
     console.log("Server is running on port 3001")
 })
