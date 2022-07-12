6cae13de312dcfcdcfaf7890c869eeb143ea640f
smirnovdv <sdmit@yandex.ru>
2022-07-12 15:06:26 -0500

    make it better

--- server.js
+++ server.js
@@ -12,11 +12,11 @@ const { Client } = require('pg');
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: true
+    rejectUnauthorized: false
   }
 });
-
-client.connect(err => {
+client.connect
+(err => {
   console.log('hi from client connect')
   if (err) {
     console.error('connection error', err.stack)
