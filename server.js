882034c027b275273d4510ca4494985ab2aa0208
smirnovdv <sdmit@yandex.ru>
2022-07-12 14:30:03 -0500

    make it better

--- server.js
+++ server.js
@@ -10,7 +10,10 @@ app.use(cors());
 const { Client } = require('pg');
 
 const client = new Client({
-  connectionString: process.env.DATABASE_URL
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: true
+  }
 });
 
 client.connect(err => {
