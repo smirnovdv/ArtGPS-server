4d28e55157bfc376ca77976b00b69361d6a3aa30
smirnovdv <sdmit@yandex.ru>
2022-07-11 22:48:45 -0500

    env added

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors')
 const port = process.env.PORT || 3001;
-const auth = require('./auth')
+const auth = process.env.DBPASSWORD || require('./auth')
 
 app.use(cors());
 
